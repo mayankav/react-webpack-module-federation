93761a30c4efe9c68b98b5a6e26795a0a6267e96
Mayank <mayankmacav@gmail.com>
2021-09-25 16:19:35 +0530

    created container deploy script - github actions workflow

--- container/webpack/webpack.prod.js
+++ container/webpack/webpack.prod.js
@@ -4,8 +4,30 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
+
+// html-webpack plugin
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const plugin_html_webpack = new HTMLWebpackPlugin({template: './public/index.html'});
+// module-federation plugin
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const plugin_module_federation = new ModuleFederationPlugin({
+    name: 'container',
+    shared: packageJson.dependencies,
+    remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+    }
+});
+
 // production specific configuration
 const prodConfig = {
     mode: 'production',
-    
+    plugins: [plugin_module_federation, plugin_html_webpack],
+    output: {
+        filename: '[name].[contenthash].js'
+    }
 }
+
+// keep prodConfig after commonConfig so that it has higher precedence
+module.exports = merge(commonConfig, prodConfig);
--- marketing/webpack/webpack.prod.js
+++ marketing/webpack/webpack.prod.js
@@ -4,8 +4,30 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
+const packageJson = require('../package.json');
+
+// html-webpack plugin
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const plugin_html_webpack = new HTMLWebpackPlugin({template: './public/index.html'});
+// module-federation plugin
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const plugin_module_federation = new ModuleFederationPlugin({
+    name: 'marketing',
+    filename: 'remoteEntry.js',
+    shared: packageJson.dependencies,
+    exposes: {
+        './MarketingApp' : './src/boot.js'
+    }
+});
+
 // production specific configuration
 const prodConfig = {
     mode: 'production',
-    
+    plugins: [plugin_module_federation, plugin_html_webpack],
+    output: {
+        filename: '[name].[contenthash].js'
+    }
 }
+
+// keep the production config after common config so that it has more precedence
+module.exports = merge(commonConfig, prodConfig);
